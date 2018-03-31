7aaff8ebccebff9986590600d6118e561136e879
Endel Dreyer <endel.dreyer@gmail.com>
2018-03-30 23:37:17 -0300

    add 'root' option. remove archiver dependency. use adm-zip for zip and unzip.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as https from "https";
 import * as fs from "fs";
 import * as path from "path";
-import * as archiver from "archiver";
 import * as AdmZip from "adm-zip";
 import * as httpServer from "http-server";
+import * as glob from "glob";
 
 const downloads = require("../data/downloads.json");
 
@@ -52,30 +52,26 @@ export function serve (options: any) {
 }
 
 export function pack (options: any) {
-    const filename = 'addon.c3addon';
     const pluginDir = getRootDir(options)
+    const addonJson = require(pluginDir + "/addon.json");
+    const filename = `${addonJson.id}.c3addon`;
 
-    const output = fs.createWriteStream(`${pluginDir}/${filename}`);
-    output.on('close', function () {
-        console.log(archive.pointer() + ' total bytes');
+    const output = fs.createWriteStream(`${path.resolve(".")}/${filename}`);
+    const zipFile = new AdmZip();
+
+    glob(`${pluginDir}/**/*.{${VALID_EXTENSIONS.join(',')}}`, (err, files) => {
+        files.forEach(file => {
+            if (file !== pluginDir) {
+                const relativeFilename = file.replace(pluginDir, "");
+                console.log("Packing...", relativeFilename);
+                zipFile.addFile(relativeFilename, fs.readFileSync(file));
+            }
+        });
+
+        output.write(zipFile.toBuffer());
+        console.log("Done.");
+        console.log(`Generated: '${filename}'`);
     });
-
-    const archive = archiver('zip', { zlib: { level: 9 } });
-
-    // catch warnings and errors
-    archive.on('warning', function (err) {
-        if (err.code === 'ENOENT') {
-            console.error(err);
-        } else {
-            throw err;
-        }
-    });
-
-    archive.on('error', (err) => { throw err });
-
-    archive.pipe(output);
-    archive.glob(`**/*.{${VALID_EXTENSIONS.join(',')}}`);
-    archive.finalize();
 }
 
 export function docs (options: any) {
